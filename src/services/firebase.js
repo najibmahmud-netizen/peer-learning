28bb2c1e7d1e5ca9109441c28cecc82845394eeb
Abu-Hafsah <najib.mahmud@student.moringaschool.com>
2026-05-14 12:38:56 +0300

    index.css fixed

---
+++ src/services/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDbjJ7fETNaZRC5DOD56gUZA-JvFsvMm_4',
+  authDomain: 'peer-learning-project-7c0bb.firebaseapp.com',
+  projectId: 'peer-learning-project-7c0bb',
+  storageBucket: 'peer-learning-project-7c0bb.appspot.com',
+  messagingSenderId: '890618271563',
+  appId: '1:890618271563:web:846d56446df4540da67ace',
+  measurementId: 'G-ETS3CPVGBZ'
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
+export const googleProvider = new GoogleAuthProvider()
+export const db = getFirestore(app)
